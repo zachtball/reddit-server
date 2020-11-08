c317784ab6d59fb5792fe6b380d01c2a4ecf62c6
ZachMac <zachtball@gmail.com>
2020-11-08 15:09:50 -0700

    add error handler for auth

--- resources/user/user.controller.js
+++ resources/user/user.controller.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const { newToken } = require('../../utils/auth');
 dotenv.config();
 
-const authenticateMe = (req, res) => {
+const authenticateMe = (req, res, next) => {
   const { code, redirect_uri } = req.body;
   request.post(
     {
@@ -19,49 +19,51 @@ const authenticateMe = (req, res) => {
     },
     (err, _, body) => {
       if (err) {
-        console.log(err);
-        res.send(err);
+        console.log({ err });
+        next(err);
         return;
       }
       const parsedBody = JSON.parse(body);
-      const r = new Snoowrap({
-        userAgent: 'writeit',
-        clientId: process.env.API_CLIENT_ID,
-        clientSecret: process.env.API_SECRET_KEY,
-        refreshToken: parsedBody.refresh_token,
-      });
-      r.getMe()
-        .then((rRes) => {
-          User.findOneAndUpdate(
-            { username: rRes.name },
-            {
-              access_token: parsedBody.access_token,
-              refresh_token: parsedBody.refresh_token,
-            },
-            { upsert: true, returnNewDocument: true }
-          )
-            .lean()
-            .exec()
-            .then((user) => {
-              // if (user) {
+      if (!parsedBody.error) {
+        const r = new Snoowrap({
+          userAgent: 'writeit',
+          clientId: process.env.API_CLIENT_ID,
+          clientSecret: process.env.API_SECRET_KEY,
+          refreshToken: parsedBody.refresh_token,
+        });
+        r.getMe()
+          .then((rRes) => {
+            User.findOneAndUpdate(
+              { username: rRes.name },
+              {
+                access_token: parsedBody.access_token,
+                refresh_token: parsedBody.refresh_token,
+              },
+              { upsert: true, returnNewDocument: true }
+            )
+              .lean()
+              .exec()
+              .then((user) => {
+                // if (user) {
 
-              // }
-              // if (!user) {
-              //   try {
-              //     User.create({
-              //       user_name: rRes.name,
-              //       access_token: parsedBody.access_token,
-              //       refresh_token: parsedBody.refresh_token,
-              //     });
-              //   } catch (e) {
-              //     console.error(e);
-              //     res.status(400).end();
-              //   }
-              // }
-              res.status(200).json(newToken(user._id));
-            });
-        })
-        .catch((err) => console.log('init auth', err));
+                // }
+                // if (!user) {
+                //   try {
+                //     User.create({
+                //       user_name: rRes.name,
+                //       access_token: parsedBody.access_token,
+                //       refresh_token: parsedBody.refresh_token,
+                //     });
+                //   } catch (e) {
+                //     console.error(e);
+                //     res.status(400).end();
+                //   }
+                // }
+                res.status(200).json(newToken(user._id));
+              });
+          })
+          .catch((err) => console.log('init auth', err));
+      }
     }
   );
 };
